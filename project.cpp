d444bac915e78a4fc5d6a6f15ed1b7ebdb4885c0
S <khalili.sara262@gmail.com>
2025-04-17 15:35:55 +0000

    git commit -m "add showAll fuction and dibug insert fuction

--- project.cpp
+++ project.cpp
@@ -6,15 +6,22 @@ class Database
 private:
     std::vector<std::string> records;
 public:
-    void insert(std::string& record){
+    void insert(const std::string& record){
         records.push_back(record);
-        std::cout << "OK\n";
+    }
+    void showAll() const{
+        std::cout <<"All record:\n";
+        size_t i=0;
+        while (i < records.size()){
+            std::cout << i << ":" << records[i] << "\n";
+            i++;
+        }
     }
     
 };
 int main(){
     Database db;
-    int chos;
+    int number=1;
     std::string record;
 
 
@@ -23,18 +30,26 @@ int main(){
     std::cout <<"2.update record\n";
     std::cout <<"3.find record\n";
     std::cout <<"4.delet record\n";
-    std::cout <<"enter number";
-    std::cin >> chos;
+    std::cout <<"5.show all\n";
+    std::cout <<"6.exit\n";
 
-    if ( chos>4  || chos<1){
-        std::cout << "Error\n";
-        return 0;
-    }
-    else if ( chos == 1){
-        std::cin.ignore();
-        std::getline (std::cin, record);
-        db.insert(record);
-    }
+    while (number!=6){
+        std::cout <<"enter number:";
+        std::cin >> number;
+       if ( number> 6  || number<1){
+           std::cout << "Error\n";
+            return 0;
+        }
+        if ( number == 1){
+            std::cout << "Enter record:\n";
+            std::cin.ignore();
+            std::getline (std::cin, record);
+            db.insert(record);
+        }
+        else if( number == 5){
+            db.showAll();
+        }
+    }    
     return 0;
 
 }
