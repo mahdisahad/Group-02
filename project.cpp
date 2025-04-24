9cb14c2cb26d2c564c0488cc8df41b5b1b6ce417
Mahdis Ahad <mahdis.ahad06@gmail.com>
2025-04-24 06:08:31 +0000

    removed std::

--- project.cpp
+++ project.cpp
@@ -29,32 +29,32 @@ int main(){
     std::string record;
 
 
-    std::cout <<"Menu:\n";
-    std::cout <<"1.insert record\n";
-    std::cout <<"2.update record\n";
-    std::cout <<"3.find   record\n";
-    std::cout <<"4.delet  record\n";
-    std::cout <<"5.show all\n";
-    std::cout <<"6.Exit\n";
+    cout <<"Menu:\n";
+    cout <<"1.insert record\n";
+    cout <<"2.update record\n";
+    cout <<"3.find   record\n";
+    cout <<"4.delet  record\n";
+    cout <<"5.show all\n";
+    cout <<"6.Exit\n";
 
     while (number!=6){
-        std::cout <<"Enter number:";
-        std::cin >> number;
+        cout <<"Enter number:";
+        cin >> number;
        if ( number> 5  || number<1){
-           std::cout << "Error\n";
+           cout << "Error\n";
             return 0;
         }
         if ( number == 1){
-            std::cout << "Enter record:\n";
-            std::cin.ignore();
-            std::getline (std::cin, record);
+            cout << "Enter record:\n";
+            cin.ignore();
+            getline (cin, record);
             db.insert(record);
         }
         else if( number == 5){
             db.showAll();
         }
         else if( number == 6)
-            std::cout << "Exiting.....\n";
+            cout << "Exiting.....\n";
     }
     
     return 0;
